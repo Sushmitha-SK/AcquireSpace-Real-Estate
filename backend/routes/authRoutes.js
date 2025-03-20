f682b47d90159c5e2c502cc890e58d8c4f174ee7
Sushmitha S <94210498+Sushmitha-SK@users.noreply.github.com>
2025-03-20 17:21:31 +0530

    Updated authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
@@ -9,5 +10,6 @@ router.post('/signout', authController.signOut);
 router.post('/resendOtp', authController.resendOtp);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password/:token', authController.resetPassword);
+router.post('/changePassword', authMiddleware, authController.changePassword);
 
 module.exports = router;
