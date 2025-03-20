d0381f41a1db12e0824bd46c203fe930bf4076ea
Sushmitha S <94210498+Sushmitha-SK@users.noreply.github.com>
2025-03-20 17:20:38 +0530

    Updated authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -289,8 +289,6 @@ const authController = {
         }
     },
 
-
-
     resetPassword: async (req, res) => {
         const { token } = req.params;
         const { password } = req.body;
@@ -315,6 +313,35 @@ const authController = {
         } catch (error) {
             console.error(`Error: ${error.message}`);
             res.status(500).send('Server Error');
+        }
+    },
+
+     changePassword: async (req, res) => {
+        const { currentPassword, newPassword } = req.body;
+
+        try {
+            const userId = req.user.id; 
+            const user = await User.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({ msg: "User not found" });
+            }
+
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Current password is incorrect" });
+            }
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+
+            await user.save();
+
+            res.json({ msg: "Password changed successfully" });
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+            res.status(500).send("Server Error");
         }
     }
 };
